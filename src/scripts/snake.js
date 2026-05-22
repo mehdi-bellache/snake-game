66089ba77de8d226c3cc9a238e270127bc76db0a
Mehdi <wassim@DESKTOP-64CJFF7.localdomain>
2026-05-22 18:45:20 +0200

    add a Snake class

---
+++ src/scripts/snake.js
@@ -0,0 +1,100 @@
+
+import imgSrc from '../assets/snake.png' ;
+
+export default class Snake{
+    #x ;
+    #y ;
+    #deltaX ;
+    #deltaY ;
+    #image ;
+
+    constructor(x, y, deltaX= 0, deltaY = 0){
+        this.#x = x ;
+        this.#y = y ; 
+        this.#deltaX = deltaX ;
+        this.#deltaY = deltaY ;
+        this.#image = imgSrc ;
+    }
+
+    get x(){
+        return this.#x ;
+    }
+
+    get y(){
+        return this.#y ;
+    }
+
+    get deltaX(){
+        return this.#deltaX ;
+    }
+
+    get deltaY(){
+        return this.#deltaY ;
+    }
+
+    get image(){
+        return this.#image ;
+    }
+
+    get width() {
+        return this.#image.width ;
+    }
+
+    get height() {
+        return this.#image.height ;
+    }
+
+    setX(value){
+        this.#x = value ;
+    }
+
+    setY(value){
+        this.#y = value ;
+    }
+
+    setDeltaX(value){
+        this.#deltaX = value ; 
+    }
+
+    setDeltaY(value){
+        this.#deltaY = value ; 
+    }
+
+    // setImage(newImgSrc){
+    //     this.#image = this.#createImage(newImgSrc) ;
+    // }
+
+    // #createImage(imageSource) {
+	//     const newImg = new Image();
+  	//     newImg.src = imageSource;
+  	//     return newImg;
+    // }
+
+
+    // draw(context){
+    //     context.drawImage(this.#image, this.#x, this.#y);
+    // }
+
+    // move(canvas){
+    //     this.#x += this.#deltaX;
+    //     this.#y += this.#deltaY;
+    // }
+
+    // collisionWith(obstacle){
+    
+    //     const P1_X = Math.max(this.#x, obstacle.#x);
+    //     const P1_Y = Math.max(this.#y, obstacle.#y) ;
+
+    //     const A2_X = this.#x + this.#image.width ;
+    //     const A2_Y = this.#y + this.#image.height ; 
+    
+    //     const P2_X = Math.min(A2_X, obstacle.#x + obstacle.#image.width);
+    //     const P2_Y = Math.min(A2_Y, obstacle.#y + obstacle.#image.height);
+
+    //     return ((P1_X < P2_X) && (P1_Y < P2_Y)) ? true : false ; 
+    
+    // }
+
+    // alea(n){ return Math.floor(Math.random()* n) ; }
+
+}
